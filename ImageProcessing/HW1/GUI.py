7e4d6c7397e4c7c406a8e0f33cfc5b5899112cef
heisenberg0424 <agent.e@icloud.com>
2020-10-12 00:49:59 +0800

    GUI added

--- ImageProcessing/HW1/GUI.py
+++ ImageProcessing/HW1/GUI.py
@@ -2,19 +2,28 @@ import tkinter
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
+from PIL import Image,ImageTk
 from PIL import ImageEnhance
+#Functions
+def load_btn_func():
+    origin_image_lbl.config(image=origin_image,width=300, height = 300)
+
 #window setting
 window = tkinter.Tk()
 window.title('DIP HW1')
-window.geometry('500*500')
+window.geometry('1280x720')
 
 #IMGlabel
-origin_image_lbl=tkinter.Label(window,image=origin_image,width=300, height = 300)
-origin_image=ImageTk.PhotoImage(load)
 load=Image.open('test.jpg')
+origin_image=ImageTk.PhotoImage(load)
+origin_image_lbl=tkinter.Label(window)
+
+
+#Button
+load_btn=tkinter.Button(window,text='LOAD',command=load_btn_func)
 
 #Pack
-origin_image_lbl.pack()
+origin_image_lbl.place(relx=0.3,rely=0.02)
+load_btn.place(relx=0.1,rely=0.1)
 
 window.mainloop()
