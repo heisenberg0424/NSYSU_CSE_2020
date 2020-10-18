fd130a179d9f58861484fd93976b6cfb8db2705b
heisenberg0424 <agent.e@icloud.com>
2020-10-18 22:57:04 +0800

    8. not done

--- Algorithm/HW1/B075040041_HW1.cpp
+++ Algorithm/HW1/B075040041_HW1.cpp
@@ -10,76 +10,54 @@ int datax[100] = {0};
 int datay[100] = {0};
 
 int main() {
-    int tempi, tempx, tempy;
-    ifstream input("readfile.txt");
-    int i, j, cnt = 0;
+    int tempi, tempx, tempy;                            //variables for input file
+    ifstream input("readfile.txt");                     //open file
+    int i, j, cnt = 0;                                  //cnt for number of data
 
-    while (input >> tempi >> tempx >> tempy) {
-        cnt++;
+    while (input >> tempi >> tempx >> tempy) {          //input data
+        cnt++;                                          //save data
         datax[tempi - 1] = tempx;
         datay[tempi - 1] = tempy;
     }
 
-    float matrix[cnt][cnt];
-    for (i = 0; i < cnt; i++) {
-        for (j = 0; j < cnt; j++) {
-            matrix[i][j] = distance(i, j);
-        }
-    }
-
-    // PRINT　MATRIX
-    /*for (i=0;i<cnt;i++){
-        for (j=0;j<cnt;j++){
-            cout<<fixed<<setprecision(3)<<matrix[i][j]<<" ";
-        }
-        cout<<endl;
-    }*/
-
-    int path[cnt];
-    for (i = 0; i < cnt; i++) {
+    int path[cnt];                                      //array to store trip order
+    for (i = 0; i < cnt; i++)
         path[i] = i;
-    }
-    float minpath = MAXFLOAT;
-    float temp;
-    int minpath_order[cnt];
-    clock_t start=clock();
+    float minpath = MAXFLOAT;                           //initialize
+    float temp;                                         //store trip distance
+    int minpath_order[cnt];                             //store min trip order
+    clock_t start=clock();                              //timer
     do {
-        temp = 0;
-        for (i = 0; i < cnt - 1; i++) {
-            temp += matrix[path[i]][path[i + 1]];
-            // cout<<temp<<endl;
-        }
-        temp+=matrix[path[10]][path[0]];
-        if (temp < minpath) {
-            minpath = temp;
-            for (i = 0; i < cnt; i++) {
+        temp = 0;                                       //initialize trip distance
+        for (i = 0; i < cnt - 1; i++)                   //calculate trip distance
+            temp += distance(path[i],path[i + 1]);
+        temp+=distance(path[cnt-1],path[0]);
+        if (temp < minpath) {                           //if trip is shorter
+            minpath = temp;                             //store min trip
+            for (i = 0; i < cnt; i++)
                 minpath_order[i] = path[i];
-            }
         }
 
-        /*for (i = 0; i < cnt; i++) {
-            cout << path[i] << " ";
-        }
-        cout << endl;*/
-
-    } while (next_permutation(path + 1, path + cnt));
+    } while (next_permutation(path + 1, path + cnt));   //loop next permutation
 
     clock_t end=clock();
     double elapsed=double(end-start)/CLOCKS_PER_SEC;
-    cout<<"Best Vist Order:";
-    for (i = 0; i < cnt; i++) {
+    cout<<"Best Vist Order:";                           //output
+    for (i = 0; i < cnt; i++)
         cout << minpath_order[i]+1 << " ";
-    }
+    
     cout << endl<<"Best Distance:"<<minpath<<endl;
     cout << "Execution Time:" <<elapsed<<endl;
 
-    ofstream output("result.txt");
-    for (i=0;i<cnt;i++){
+    ofstream output("result.txt");                      //output for GNUplot
+    for (i=0;i<cnt;i++)
         output<<datax[minpath_order[i]]<<" "<<datay[minpath_order[i]]<<endl;
-    }
+    
     output<<datax[minpath_order[0]]<<" "<<datay[minpath_order[0]]<<endl;
+    
+    system("");
 }
 
-float distance(int x, int y) {
+float distance(int x, int y) {                          //calculate distance between two points
     return sqrt(pow((datax[x] - datax[y]), 2) + pow((datay[x] - datay[y]), 2));
 }
