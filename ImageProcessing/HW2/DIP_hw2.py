2717404242b258da5c038a89ef508030ab9bd884
heisenberg0424 <agent.e@icloud.com>
2020-10-31 01:33:48 +0800

    fist push

---
+++ ImageProcessing/HW2/DIP_hw2.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+#grey scale slicing
+image=cv2.imread('test2.tif',0)
+print(image.shape)
+x,y=image.shape
+z=np.zeros((x,y))
+for i in range(0,x):
+    for j in range(0,y):
+        if(image[i][j]>100 and image[i][j]<150):
+            z[i][j]=255
+        else:
+            z[i][j]=0
+equ=np.hstack((image,z))
+plt.title('Original\Graylevel slicing w/o background')
+plt.imshow(equ,'gray')
+plt.show()
+
+#bit slicing
+# Read the image in greyscale
+img = cv2.imread('test2.tif',0)
+ 
+#Iterate over each pixel and change pixel value to binary using np.binary_repr() and store it in a list.
+lst = []
+for i in range(img.shape[0]):
+    for j in range(img.shape[1]):
+         lst.append(np.binary_repr(img[i][j] ,width=8)) # width = no. of bits
+ 
+# We have a list of strings where each string represents binary pixel value. To extract bit planes we need to iterate over the strings and store the characters corresponding to bit planes into lists.
+# Multiply with 2^(n-1) and reshape to reconstruct the bit image.
+eight_bit_img = (np.array([int(i[0]) for i in lst],dtype = np.uint8) * 128).reshape(img.shape[0],img.shape[1])
+seven_bit_img = (np.array([int(i[1]) for i in lst],dtype = np.uint8) * 64).reshape(img.shape[0],img.shape[1])
+six_bit_img = (np.array([int(i[2]) for i in lst],dtype = np.uint8) * 32).reshape(img.shape[0],img.shape[1])
+five_bit_img = (np.array([int(i[3]) for i in lst],dtype = np.uint8) * 16).reshape(img.shape[0],img.shape[1])
+four_bit_img = (np.array([int(i[4]) for i in lst],dtype = np.uint8) * 8).reshape(img.shape[0],img.shape[1])
+three_bit_img = (np.array([int(i[5]) for i in lst],dtype = np.uint8) * 4).reshape(img.shape[0],img.shape[1])
+two_bit_img = (np.array([int(i[6]) for i in lst],dtype = np.uint8) * 2).reshape(img.shape[0],img.shape[1])
+one_bit_img = (np.array([int(i[7]) for i in lst],dtype = np.uint8) * 1).reshape(img.shape[0],img.shape[1])
+ 
+#Concatenate these images for ease of display using cv2.hconcat()
+finalr = cv2.hconcat([eight_bit_img,seven_bit_img,six_bit_img,five_bit_img])
+finalv =cv2.hconcat([four_bit_img,three_bit_img,two_bit_img,one_bit_img])
+ 
+# Vertically concatenate
+final = cv2.vconcat([finalr,finalv])
+ 
+# Display the images
+cv2.imshow('a',final)
+cv2.waitKey(0)
