ae82927fe3e25de270e47959af31abd68fa931d8
heisenberg0424 <agent.e@icloud.com>
2020-11-10 15:40:02 +0800

    OOP HW3

---
+++ OOP/HW3/Complex.cpp
@@ -0,0 +1,94 @@
+#include "Complex.h"
+#include <cmath>
+using namespace std;
+double pi=M_PI;
+
+double Complex::Real(){return real;}
+double Complex::Imag(){return imag;}
+
+double Complex::Norm(){return real*real+imag*imag;}
+double Norm(const Complex &x){return x.real*x.real+x.imag*x.imag;}
+
+double Complex::Abs(){return sqrt(Norm());}
+double Abs(const Complex &x){return sqrt(Norm(x));}
+
+double Complex::Arg(){
+    if(imag!=0){
+        return 2*atan(imag/(Abs()+real));
+    }
+    else if (imag>0)
+        return 0;
+    else 
+        return pi;
+}
+double Arg(const Complex &x){
+    Complex temp(x);
+    return temp.Arg();
+}
+
+ostream& operator<<(ostream &o,const Complex &x){
+    o<<'('<<x.real<<','<<x.imag<<')';
+    return o;
+}
+
+Complex::Complex(const double re,const double im){
+    real=re;
+    imag=im;
+}
+
+Complex::Complex(const Complex &c){
+    real=c.real;
+    imag=c.imag;
+}
+
+Complex& Complex::operator=(const Complex &c){
+    this->real=c.real;
+    this->imag=c.imag;
+    return *this;
+}
+
+Complex Complex::operator++(){
+    real++;
+    imag++;
+    return *this;
+}
+Complex Complex::operator++(int){
+    Complex temp=*this;
+    real++;
+    imag++;
+    return temp;
+}
+
+Complex Complex::Polar(const double leng,const double arg){
+    this->real=leng*cos(arg);
+    this->imag=leng*sin(arg);
+    return *this;
+}
+Complex Polar(const double leng,const double arg){
+    Complex *temp=new Complex();
+    temp->imag=leng*sin(arg);
+    temp->real=leng*cos(arg);
+    return *temp;
+}
+
+Complex operator+(const Complex &x,const Complex &y){
+    Complex *temp=new Complex(x.real+y.real,y.imag+x.imag);
+    return *temp;
+}
+Complex operator-(const Complex &x,const Complex &y){
+    Complex *temp=new Complex(x.real-y.real,x.imag-y.imag);
+    return *temp;
+}
+Complex operator*(const Complex &x,const Complex &y){
+    Complex *temp=new Complex();
+    temp->real=(x.real)*(y.real)-(x.imag)*(y.imag);
+    temp->imag=(x.real)*(y.imag)+(y.real)*(x.imag);
+    return *temp;
+}
+Complex operator/(const Complex &x,const Complex &y){
+    
+}
+
+Complex::~Complex(){}
+
+
